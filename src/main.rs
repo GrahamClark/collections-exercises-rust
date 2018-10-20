0797b0ab1a74b5b5ff760bb069b399d5b3708dfa
Graham Clark <grahamclarkuk@gmail.com>
2018-10-20 22:32:09 +0100

    renamed project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-extern crate hash_maps;
+extern crate collections_exercises;
 
 use std::collections::HashMap;
 
 fn main() {
     let v = vec![1, 45, 73, 32, 18];
-    let mean = hash_maps::mean(v);
+    let mean = collections_exercises::mean(v);
     println!("mean = {}", mean);
 
-    println!("apple = {}", hash_maps::pig_latin("apple"));
-    println!("apple = {}", hash_maps::pig_latin("first"));
+    println!("apple = {}", collections_exercises::pig_latin("apple"));
+    println!("apple = {}", collections_exercises::pig_latin("first"));
 
     let mut departments :HashMap<String, Vec<String>> = HashMap::new();
-    departments = hash_maps::staff_to_dept("Add Sally to Engineering", departments);
-    departments = hash_maps::staff_to_dept("Add Amir to Sales", departments);
-    departments = hash_maps::staff_to_dept("Add Ben to Engineering", departments);
+    departments = collections_exercises::staff_to_dept("Add Sally to Engineering", departments);
+    departments = collections_exercises::staff_to_dept("Add Amir to Sales", departments);
+    departments = collections_exercises::staff_to_dept("Add Ben to Engineering", departments);
 
     println!("{:?}", departments);
 }
